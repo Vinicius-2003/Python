e93f6d5a1e80d480d6028f93b87dc622e6e61352
Vinicius Silva Almeida <viniciusdobrazil@gmail.com.br>
2023-07-03 20:53:46 -0300

    update hangman.py

--- Jogo/Hangman.py
+++ Jogo/Hangman.py
@@ -3,24 +3,38 @@ def playing():
     print(' Hangman Game')
     print('*' * 20)
 
-    secret_word = "banana"
-    correct_letters = ["_", "_", "_", "_", "_", "_"]
+    secret_word = "banana".upper()
+    correct_letters = ["_" for letter in secret_word]
 
-    hanged = False
-    correct = False    
+    errors = 0
 
-    while (not correct and not hanged):
+    print(correct_letters)
+
+    while (True):
 
         guessing_letters = input("Qual letra? ")
-        guessing_letters = guessing_letters.strip()
+        guessing_letters = guessing_letters.strip().upper()
+        if(guessing_letters in secret_word):
+            index = 0
+            for letter in secret_word:
+                if (guessing_letters == letter):
+                    correct_letters[index] = letter
+                index += 1
+        else:
+            errors += 1
+            print("Você têm {} de {}".format(6 - errors,len(secret_word)))
 
-        index = 0
-        for letra in secret_word:
-            if (guessing_letters.upper() == letra.upper()):
-                correct_letters[index] = letra
-            index = index + 1
-
+        if(errors == 6):
+            break
+        if("_" not in correct_letters):
+            break
         print(correct_letters)
+
+    if("_" not in correct_letters):
+        print('Você ganhou o jogo!')
+    else:
+        print("Você perdeu :/")
+
     print('Game over')
 
 if(__name__ == "__main__"):
