76d1aa2345649bf3a35a84c5c583638e52cdc876
Vinícius Almeida <viniciusdobrazil@gmail.com>
2025-02-20 10:27:42 -0300

    Update no exercicio de Tupla Aleatória

--- Mundo_3/TuplaAleatoria.py
+++ Mundo_3/TuplaAleatoria.py
@@ -1,25 +1,13 @@
-import random
-
-# Cria uma lista
-lista_numeros_aleatorios = []
-
-# Criar uma lista com os valores gerados aleatoriamente
-for i in range(5):
-    numeros_aleatorios = random.randint(1,5)
-    lista_numeros_aleatorios.append(numeros_aleatorios)
-
-# Cria uma tupla, e adiciona os valores da lista na tupla
-tupla_numeros_aleatorios = (lista_numeros_aleatorios)
-print(tupla_numeros_aleatorios)
+from random import randint
+import os
 
 
+os.system('cls')
+tupla = (randint(1,100),randint(1,100),randint(1,100),randint(1,100),randint(1,100))
 
-# Maior valor
-for a in range(0,4):
-    for b in range (1,5):
-        if a >= b:
-            maior = int(a)
-        elif a <= b:
-            maior = int(b)
-            
-print(maior)
+print("Os valores sorteados foram: ", end='')
+for n in tupla: 
+    print(f" {n} ", end='')
+    
+print(f"\nO maior valor foi de: {max(tupla)}")
+print(f"O menor valor foi de : {min(tupla)}")
